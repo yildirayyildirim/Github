c391df9c3e825405b629b310eaf5273540ed22ee
yildiray.yildirim <yildiray.yildirim@pixelplus.net>
2021-06-23 22:20:52 +0300

    #230620212230 finish proje

--- app/src/main/java/com/app/github/presentation/view/fragment/DetailFragment.kt
+++ app/src/main/java/com/app/github/presentation/view/fragment/DetailFragment.kt
@@ -5,6 +5,7 @@ import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.app.github.R
 import com.app.github.core.base.BaseFragment
+import com.app.github.core.persistence.entity.BookmarkEntity
 import com.app.github.databinding.ViewFragmentDetailBinding
 import com.app.github.presentation.viewmodel.DetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DetailFragment : BaseFragment<ViewFragmentDetailBinding, DetailViewModel>() {
     private val args: DetailFragmentArgs by navArgs()
+    private var isFavorite: Boolean = false
 
     override fun getContentView(): Int {
         return R.layout.view_fragment_detail
@@ -28,9 +30,33 @@ class DetailFragment : BaseFragment<ViewFragmentDetailBinding, DetailViewModel>(
     }
 
     private fun init() {
+        baseViewModel.load(args.model.id)
+        baseViewModel.bookmarkEntity.observe(viewLifecycleOwner, {
+            isFavorite = false
+            it?.let {
+                isFavorite = true
+            }
+            dataBinding.isFavorite =isFavorite
+        })
+
         dataBinding.imgBack.setOnClickListener {
             val action = DetailFragmentDirections.actionDetailFragmentToHomeFragment()
             Navigation.findNavController(it).navigate(action)
+        }
+
+        dataBinding.imgStars.setOnClickListener {
+            baseViewModel.insertAndRemove(
+                isFavorite,
+                BookmarkEntity(
+                    args.model.id,
+                    args.model.owner.avatar_url,
+                    args.model.name,
+                    args.model.description,
+                    args.model.stargazers_count,
+                    args.model.owner.login,
+                    "0"
+                )
+            )
         }
     }
 }
--- app/src/main/java/com/app/github/presentation/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/app/github/presentation/viewmodel/DetailViewModel.kt
@@ -1,13 +1,48 @@
 package com.app.github.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import com.app.github.core.base.BaseViewModel
+import com.app.github.core.persistence.entity.BookmarkEntity
 import com.app.github.core.repository.BookmarkRepository
-import com.app.github.core.repository.GithubRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel
 @Inject
 constructor(private val repository: BookmarkRepository) : BaseViewModel() {
+    var bookmarkEntity: LiveData<BookmarkEntity>
+
+    init {
+        bookmarkEntity = repository.getBookmarkEntityById(0)
+    }
+
+    fun load(id: Int) {
+        getIsLoading().value = true
+        bookmarkEntity = repository.getBookmarkEntityById(id)
+        getIsLoading().value = false
+    }
+
+    private fun insert(bookmarkEntity: BookmarkEntity) = viewModelScope.launch {
+        getIsLoading().value = true
+        repository.insert(bookmarkEntity)
+        getIsLoading().value = false
+    }
+
+    private fun remove(bookmarkEntity: BookmarkEntity) = viewModelScope.launch {
+        getIsLoading().value = true
+        repository.delete(bookmarkEntity)
+        getIsLoading().value = false
+    }
+
+    fun insertAndRemove(isFavorite: Boolean, bookmarkEntity: BookmarkEntity) {
+        if (isFavorite) {
+            remove(bookmarkEntity)
+        } else {
+            insert(bookmarkEntity)
+        }
+        load(bookmarkEntity.id)
+    }
 }
